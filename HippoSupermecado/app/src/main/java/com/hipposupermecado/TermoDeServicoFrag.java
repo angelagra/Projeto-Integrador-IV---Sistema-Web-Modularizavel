7693eedddc582df152e330f762d6823c43b485c3
markgoerlich <markgoerlich0@gmail.com>
2018-04-17 20:23:05 -0300

    Funcionalidades Termo
    Implantada Funcionalidades no Checkbox do termo de serviço

--- HippoSupermecado/app/src/main/java/com/hipposupermecado/TermoDeServicoFrag.java
+++ HippoSupermecado/app/src/main/java/com/hipposupermecado/TermoDeServicoFrag.java
@@ -4,105 +4,65 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TermoDeServicoFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TermoDeServicoFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TermoDeServicoFrag extends Fragment {
+
+public class TermoDeServicoFrag extends AppCompatActivity {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RadioButton radioTermo;
+    private Button btnTermo;
 
-    private OnFragmentInteractionListener mListener;
-
-    public TermoDeServicoFrag() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TermoDeServicoFrag.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TermoDeServicoFrag newInstance(String param1, String param2) {
-        TermoDeServicoFrag fragment = new TermoDeServicoFrag();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    // TODO: Rename and change types and number of parameters
+//    public static TermoDeServicoFrag newInstance(String param1, String param2) {
+//        TermoDeServicoFrag fragment = new TermoDeServicoFrag();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+        radioTermo = findViewById(R.id.radioTermo);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (radioTermo.isChecked()) {
+                    showMessage("BEM VINDO!", "Ola Sr. ");
+                }
+
+            }
+        };
+
+        btnTermo.setOnClickListener(listener);
+
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_termo_de_servico, container, false);
-    }
+    private void showMessage(String titulo, String mensagem) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(TermoDeServicoFrag.this);
+        builder.setTitle(titulo);
+        builder.setMessage(mensagem);
+        builder.setCancelable(false);// impede de fechar em qualquer outro lugar a não ser o OK
+        builder.setPositiveButton("OK", null);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        AlertDialog dialog = builder.create();
+        dialog.show();//mostra o dialogo na tela
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
