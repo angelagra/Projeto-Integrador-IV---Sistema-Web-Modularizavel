00ab19187bb32539868e753cff6a7bc82fa5edb1
Angela Graciete <angelagraciete84@gmail.com>
2018-04-17 21:20:28 -0300

    Lista de Produtos - WEbService Consulta

--- WebServiceGrupo1/src/main/java/Lista/Produtos.java
+++ WebServiceGrupo1/src/main/java/Lista/Produtos.java
@@ -2,6 +2,66 @@
 package Lista;
 
 
-public class Produtos {
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Produtos implements Serializable {
+    
+    private Long id, idCategoria;
+    private String nome, descricao ;
+    private double preco, desconto;
+    private int qtdMinEstoque;
+    //private   imagem;
+    
+    protected Produtos(){
+        
+    }
+    
+    public Produtos(Long id, String nome, String descricao, double preco, double desconto, int qtdMinEstoque){
+        
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.desconto = desconto;
+        this.qtdMinEstoque = qtdMinEstoque;
+    }
+           
+    @XmlAttribute
+    public Long getId(){
+        return id;
+    }
+    
+    public void setId(Long id){
+        this.id = id;
+    }
+    
+    @XmlElement
+    public String getNome(){
+        return nome;
+    }
+    
+    @XmlElement
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    @XmlElement
+    public Double getPreco(){
+        return preco;
+    }
+    
+    @XmlElement
+    public Double getDesconto(){
+        return desconto;
+    }
+    
+    @XmlElement
+    public int getQtdEstoque(){
+        return qtdMinEstoque;
+    }
     
 }
---
+++ WebServiceGrupo1/src/main/java/Resources/ProdutosResources.java
@@ -0,0 +1,58 @@
+
+package Resources;
+
+import Lista.Produtos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/produtos")
+public class ProdutosResources {
+    
+    private static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static final String URL = "jdbc:sqlserver://hippo-pi.database.windows.net;database=hippo";
+    private static final String USER = "TSI";
+    private static final String PASS = "SistemasInternet123";
+    
+    @GET
+    @Produces("application/json")
+    public Response getProdutos() throws ClassNotFoundException, SQLException{
+        
+        Response response = null;
+        Class.forName(DRIVER); // carrega o driver
+        
+        Connection con = DriverManager.getConnection(URL,USER,PASS); //  conecta ao banco
+        
+        PreparedStatement stmt = con.prepareStatement("select * from produto order by nomeProduto"); // prepara
+        
+        ResultSet rs = stmt.executeQuery(); // usar executeQuery apara se for consultar
+        
+        List<Produtos> produtos = new ArrayList<Produtos>();
+        
+        while(rs.next()){
+            Long id = rs.getLong("idProduto");
+            String nome = rs.getString("nomeProduto");
+            String descricao = rs.getString("descProduto");
+            double preco = rs.getDouble("precProduto");
+            double desconto = rs.getDouble("descontoPromocao");
+            int estoque = rs.getInt("qtdMinEstoque");
+            
+            Produtos p = new Produtos(id, nome, descricao, preco, desconto, estoque);
+            produtos.add(p);
+            
+        }
+        
+        response = Response.ok(produtos).build();
+        return response;
+        
+    }
+    
+}
