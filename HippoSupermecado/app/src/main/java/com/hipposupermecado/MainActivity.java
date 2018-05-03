e66d1db82d3f038cd83a4166adb3bf901af49a17
SaulloSAM <saullo.sam115@gmail.com>
2018-05-02 22:30:26 -0300

    Menu - 90%

--- HippoSupermecado/app/src/main/java/com/hipposupermecado/MainActivity.java
+++ HippoSupermecado/app/src/main/java/com/hipposupermecado/MainActivity.java
@@ -25,18 +25,37 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         // Binding dos elementos do menu
-        navigationView = findViewById(R.id.navegation_view);
+        navigationView = (NavigationView) findViewById(R.id.navegation_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-                if (item.isCheckable()){ item.setChecked(false); }
-                else { item.setChecked(true); }
+                if (menuItem.isChecked()){ menuItem.setChecked(false); }
+                else { menuItem.setChecked(true); }
 
                 drawerLayout.closeDrawers();
+
+                if(menuItem.getItemId() == R.id.action_newUser){
+                    CadastroUsuario newUser = new CadastroUsuario();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, newUser).commit();
+                    return true;
+                }
+
                 return false;
             }
         });
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer, R.string.closeDrawer){};
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    public boolean onOptionItemSelected (MenuItem item){
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
