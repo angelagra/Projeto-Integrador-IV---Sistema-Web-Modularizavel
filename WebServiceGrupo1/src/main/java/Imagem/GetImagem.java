bde16a0e9bd01e0c54e86daeab0096259c739c1c
leopiscitelli <38262522+leopiscitelli@users.noreply.github.com>
2018-05-22 20:37:29 -0300

    ImageIO 90%

--- WebServiceGrupo1/src/main/java/Imagem/GetImagem.java
+++ WebServiceGrupo1/src/main/java/Imagem/GetImagem.java
@@ -6,15 +6,23 @@
 package Imagem;
 
 import Lista.Produtos;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -40,12 +48,25 @@ public class GetImagem extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+    
+         URL url = null;
+        
      
         String id = request.getParameter("id");
+        Integer width = Integer.parseInt(request.getParameter("w"));
+        
         Long lId = new Long (id);
         try {
             Connection conn = getConnection();
             PreparedStatement stmt = conn.prepareStatement("select imagem from Produto where idProduto  = ?");
+            
+          
+           
+        
+            
+            
+            
             stmt.setLong(1, lId.longValue());
             ResultSet rs = stmt.executeQuery();
             
@@ -53,13 +74,16 @@ public class GetImagem extends HttpServlet {
                  InputStream in = rs.getBinaryStream("imagem");
                  response.setHeader("contentType", "image/jpeg");
                  OutputStream out = response.getOutputStream();
-
+                 BufferedImage data= resize(in, width);
+                 ImageIO.write (data, "jpg", out);
+                /*
                 // Copy the contents of the file to the output stream
                  byte[] buf = new byte[1024];
                  int count = 0;
                  while ((count = in.read(buf)) >= 0) {
                    out.write(buf, 0, count);
-                }
+                }*/
+               
                 out.close();
                 in.close();
                  
@@ -72,6 +96,34 @@ public class GetImagem extends HttpServlet {
         
     }
 
+    public BufferedImage resize (InputStream imagem, int width) {
+         
+         BufferedImage dimg = null;
+        try {
+            BufferedImage originalImage = ImageIO.read(imagem);
+
+             int w = originalImage.getWidth();
+            int h = originalImage.getHeight();
+
+            float razao = (float)w/ (float)h;
+
+            int height = (int) (width / razao);
+
+
+            dimg = new BufferedImage(width, height, originalImage.getType());
+            Graphics2D g = dimg.createGraphics();
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.drawImage(originalImage, 0, 0, width, height, 0, 0, w, h, null);
+            g.dispose();
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            return dimg;
+            
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
