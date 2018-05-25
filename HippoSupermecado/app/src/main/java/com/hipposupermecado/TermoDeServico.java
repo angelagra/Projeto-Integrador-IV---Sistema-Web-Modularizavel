be0f885fc0f748ebc5e21e5692b13c1f149d206e
SaulloSAM <saullo.sam115@gmail.com>
2018-05-24 22:34:01 -0300

    Esqueci de subir uma coisinha.

--- HippoSupermecado/app/src/main/java/com/hipposupermecado/TermoDeServico.java
+++ HippoSupermecado/app/src/main/java/com/hipposupermecado/TermoDeServico.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class TermoDeServico extends AppCompatActivity {
     private RadioButton radioTermo;
-    private Button btnTermo;
+    private Button btnTermo, btnFechar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,7 @@ public class TermoDeServico extends AppCompatActivity {
 
         radioTermo = findViewById(R.id.radioTermo);
         btnTermo = findViewById(R.id.btnTermo);
+        btnFechar = findViewById(R.id.btnFechar);
 
         SharedPreferences sp = getSharedPreferences("radioTermo",MODE_PRIVATE);
         Boolean termo = sp.getBoolean("checked", false);
@@ -39,12 +40,22 @@ public class TermoDeServico extends AppCompatActivity {
                 Intent intent = new Intent(TermoDeServico.this,MainActivity.class);
                 startActivity(intent);
             } else {
-                Toast toast = Toast.makeText(TermoDeServico.this, "Infelizmente você não concorda com os nosso termos. Estamos fechando o Aplicativo.", Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(TermoDeServico.this, "Para continuar a navegação, leia e confirme os termos.", Toast.LENGTH_LONG);
                 toast.show();
-                finish();
+                return;
             }
             }
         };
         btnTermo.setOnClickListener(listener);
+
+        View.OnClickListener fechar = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(TermoDeServico.this, "Infelizmente você não concorda com os nosso termos. Estamos fechando o Aplicativo.", Toast.LENGTH_LONG);
+                toast.show();
+                finish();
+            }
+        };
+        btnFechar.setOnClickListener(fechar);
     }
 }
